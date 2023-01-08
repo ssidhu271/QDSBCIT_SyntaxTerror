e13e654e67ef50a08b20ce25fb6a60a77b001ff7
Sungmok Cho <chosm7489@gmail.com>
2023-01-07 16:45:00 -0800

    added revenue.html

---
+++ genre.js
@@ -0,0 +1,13 @@
+fetch("../archive/Game_data.json")
+  .then((response) => response.json())
+  .then((data) => {
+    // loop through the array of objects
+    data.forEach((item) => {
+      // create a new row for each object
+      let row = document.createElement("tr");
+      // add the ID, name, and age cells to the row
+      row.innerHTML = `<td>${item.Game}</td><td>${item.Revenue}</td><td>${item.Initial_release}</td><td>${item.Publisher}</td><td>${item.Genre}</td>`;
+      // append the row to the table
+      document.getElementById("myTable").appendChild(row);
+    });
+  });
